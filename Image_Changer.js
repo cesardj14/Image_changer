10a2e4f721f718b59e94de52a1e8b9d007500101
Cesar De Jongh <Cesar De Jongh>
2016-06-30 17:09:24 -0400

    Ultimos cambios

--- Image_Changer.js
+++ Image_Changer.js
@@ -1,6 +1,16 @@
 var myImage = document.querySelector('img');
 
-myImage.onclick = function() {
+myImage.onmouseover = function() {
+	var mySrc = myImage.getAttribute('src');
+
+	if(mySrc === 'Images/Jellyfish.jpg') {
+		myImage.setAttribute('src', 'Images/Hydrangeas.jpg');
+	} else {
+		myImage.setAttribute('src', 'Images/Jellyfish.jpg');
+	}
+}
+
+myImage.onmouseout = function() {
 	var mySrc = myImage.getAttribute('src');
 
 	if(mySrc === 'Images/Jellyfish.jpg') {

df2ff22ae3ac18f9278c6c25ff458b3344ad4139
Cesar De Jongh <Cesar De Jongh>
2016-06-30 16:44:24 -0400

    cosas de js

---
+++ Image_Changer.js
@@ -0,0 +1,11 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+	var mySrc = myImage.getAttribute('src');
+
+	if(mySrc === 'Images/Jellyfish.jpg') {
+		myImage.setAttribute('src', 'Images/Hydrangeas.jpg');
+	} else {
+		myImage.setAttribute('src', 'Image/Jellyfish.jpg');
+	}
+}
